7f0544d681cd92be135c651e3afe79c50e84503b
harpiya <yasir@harpiya.com>
2019-01-24 11:13:06 +0300

    ups

--- awesome_cart/compat/frappe/__init__.py
+++ awesome_cart/compat/frappe/__init__.py
@@ -1,4 +1,9 @@
-from __future__ import unicode_literals
-from awesome_cart.compat import find_compat_module
-
-globals().update(find_compat_module('frappe').__dict__)
+# @Author: Saadettin Yasir AKEL <developer>
+# @Date:   2019-01-24T10:41:06+03:00
+# @Email:  yasir@harpiya.com
+# @Project: Harpiya Kurumsal Yönetim Sistemi
+# @Filename: __init__.py
+# @Last modified by:   developer
+# @Last modified time: 2019-01-24T11:12:55+03:00
+# @License: MIT License. See license.txt
+# @Copyright: Harpiya Yazılım Teknolojileri
---
+++ awesome_cart/compat/frappe/develop/__init__.py
@@ -0,0 +1,9 @@
+# @Author: Saadettin Yasir AKEL <developer>
+# @Date:   2019-01-24T10:41:06+03:00
+# @Email:  yasir@harpiya.com
+# @Project: Harpiya Kurumsal Yönetim Sistemi
+# @Filename: __init__.py
+# @Last modified by:   developer
+# @Last modified time: 2019-01-24T11:12:42+03:00
+# @License: MIT License. See license.txt
+# @Copyright: Harpiya Yazılım Teknolojileri
